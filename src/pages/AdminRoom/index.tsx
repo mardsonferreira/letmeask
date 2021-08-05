ae9e73fafd0e90e58dca47bb5ca6ae4a15f7ed39
mardsonferreira <mardsonferreira25@gmail.com>
2021-08-04 22:16:27 -0300

    Adding close room method

--- src/pages/AdminRoom/index.tsx
+++ src/pages/AdminRoom/index.tsx
@@ -1,6 +1,7 @@
-import { useParams } from 'react-router-dom';
+import { useHistory, useParams } from 'react-router-dom';
 
 import logoImg from '../../assets/images/logo.svg';
+import deleteImg from '../../assets/images/delete.svg';
 
 import { Button } from '../../components/Button';
 import { Question } from '../../components/Question';
@@ -25,10 +26,25 @@ type RoomParams = {
 
 export function AdminRoom() {
     const { user } = useAuth();
+    const history = useHistory();
     const params = useParams<RoomParams>();
     const roomId = params.id;
 
     const {title, questions} = useRoom(roomId);
+
+    async function handleEndRoom() {
+        await database.ref(`rooms/${roomId}`).update({
+            closedAt: new Date(),
+        });
+
+        history.push('/');
+    }
+
+    async function handleDeleteQuestion(questionId: string) {
+       if (window.confirm('Are you sure you want to delete this question')) {
+           await database.ref(`rooms/${roomId}/questions/${questionId}`).remove();
+       }
+    }
 
     return (
         <Container>
@@ -37,7 +53,7 @@ export function AdminRoom() {
                     <img src={logoImg} alt="Logo" />
                     <div>
                         <RoomCode code={roomId} />
-                        <Button outlined>Close room</Button>
+                        <Button outlined onClick={handleEndRoom}>Close room</Button>
                     </div>
                 </Content>
             </Header>
@@ -51,7 +67,11 @@ export function AdminRoom() {
                 <QuestionList>
                     {questions.map(question => {
                         return (
-                            <Question key={question.id} content={question.content} author={question.author}/>
+                            <Question key={question.id} content={question.content} author={question.author}>
+                                <button type="button" onClick={() => handleDeleteQuestion(question.id)}>
+                                    <img src={deleteImg} alt="delete question" />
+                                </button>
+                            </Question>
                         )
                     })}
                 </QuestionList>
